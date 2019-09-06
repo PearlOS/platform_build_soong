7c4834570a77dd5bc11a43d9c66c9369e0635de3
Simon Shields <simon@lineageos.org>
2019-09-06 20:38:44 +0700

    soong: extend with pearl board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of Lineage variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"pearl/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -124,6 +126,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include Pearl variables
+		Pearl android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -287,6 +292,9 @@ type productVariables struct {
 	ProductHiddenAPIStubsTest   []string `json:",omitempty"`
 
 	TargetFSConfigGen []string `json:",omitempty"`
+
+	// include Pearl variables
+	Pearl android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
