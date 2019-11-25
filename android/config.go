saxmydix/android_build_soong

soong: Add function to return camera parameters library name
Change-Id: I3113ea6813b82c3f6b250205080030ff9c2255ec

android/config.go
@@ -882,6 +882,10 @@ func (c *deviceConfig) DeviceKernelHeaderDirs() []string {
 	return c.config.productVariables.DeviceKernelHeaders
 }
 
+func (c *deviceConfig) SpecificCameraParametersLibrary() string {
+	return String(c.config.productVariables.Du.Specific_camera_parameter_library)
+}
+
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.productVariables.NativeCoverage)
 }

PearlOS/platform_build_soong

android/config.go
@@ -882,6 +882,10 @@ func (c *deviceConfig) DeviceKernelHeaderDirs() []string {
 	return c.config.productVariables.DeviceKernelHeaders
 }
 
+func (c *deviceConfig) SpecificCameraParametersLibrary() string {
+	return String(c.config.productVariables.Pearl.Specific_camera_parameter_library)
+}
+
 func (c *deviceConfig) NativeCoverageEnabled() bool {
 	return Bool(c.config.productVariables.NativeCoverage)
 }